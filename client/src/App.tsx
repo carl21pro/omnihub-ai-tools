bebee9e4660f27a792be1db3330076961d6af155
Jerobie lauglaug <carlthecow117@gmail.com>
2026-07-12 17:45:54 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,1 +1,19 @@
+import { Route, Switch } from 'wouter'
+import Home from './pages/Home'
+import Tools from './pages/Tools'
+import NotFound from './pages/NotFound'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background text-foreground">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/tools" component={Tools} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  )
+}
+
+export default App
 
